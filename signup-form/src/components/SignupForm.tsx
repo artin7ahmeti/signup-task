a1a7394dc0a8740735b60cf84c40264be4eb5f85
Artin Ahmeti <artin.ahmeti10@gmail.com>
2025-01-16 19:47:40 +0100

    chore: imported Sign Up form to index.tsx

--- signup-form/src/components/SignupForm.tsx
+++ signup-form/src/components/SignupForm.tsx
@@ -107,3 +107,4 @@ const SignupForm: React.FC = () => {
         </div>
 )
 }
+export default SignupForm;
--- signup-form/src/index.tsx
+++ signup-form/src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignupForm from './components/SignupForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SignupForm />
   </React.StrictMode>
 );
 
