ba08a8b87bc8417201b5bb1ce02c5425e5c442f4
Artin Ahmeti <artin.ahmeti10@gmail.com>
2025-01-16 22:55:43 +0100

    feat: add store to index.tsx and import react-redux

--- signup-form/src/index.tsx
+++ signup-form/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import store from "./store";
 import './index.css';
 import App from './App';
 import SignupForm from './components/SignupForm';
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+      <Provider store={store}>
     <SignupForm />
+      </Provider>
   </React.StrictMode>
 );
 
