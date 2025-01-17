1c6611c0ffd2bb9986cb1ea869a1a294558e95f3
Artin Ahmeti <artin.ahmeti10@gmail.com>
2025-01-17 17:40:03 +0100

    test: unit tests for handling API responses

--- signup-form/src/__tests__/signupSlice.test.ts
+++ signup-form/src/__tests__/signupSlice.test.ts
@@ -49,5 +49,102 @@ describe('signupSlice', () => {
             expect(state.success).toBe(false);
             expect(state.error).toBe(null);
         });
+        test("should handle fulfilled state", () => {
+            const action = { type: signupUser.fulfilled.type};
+            const state = signupReducer(initialState, action);
+
+            expect(state.loading).toBe(false);
+            expect(state.success).toBe(true);
+        });
+
+        test("should handle rejected state", () => {
+            const action = { type: signupUser.rejected.type, payload: "Error occurred"};
+            const state = signupReducer(initialState, action);
+
+            expect(state.loading).toBe(false);
+            expect(state.success).toBe(false);
+            expect(state.error).toBe("Error occurred");
+        });
+
+        test("should call the API and handle fulfilled response", async() => {
+            fetchMock.mockResponseOnce(JSON.stringify({message: "Success"}), {status: 200});
+
+            const store = configureStore({
+                reducer: signupReducer,
+            });
+            const result = await store.dispatch(
+                signupUser({
+                    user_type: "researcher",
+                    first_name: "Artin",
+                    last_name: "Ahmeti",
+                    username: "ahmetiartin143",
+                    email: "artini13@hotmail.com",
+                    password: "strongpass123",
+                    country: "Albania",
+                }) as any
+            );
+
+            const state = store.getState();
+            expect(fetchMock).toHaveBeenCalledWith("https://django-dev.aakscience.com/signup/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    user_type: "researcher",
+                    first_name: "Artin",
+                    last_name: "Ahmeti",
+                    username: "ahmetiartin143",
+                    email: "artini13@hotmail.com",
+                    password: "strongpass123",
+                    country: "Albania",
+                }),
+            });
+
+            expect(state.loading).toBe(false);
+            expect(state.success).toBe(true);
+            expect(result.type).toBe("signup/signupUser/fulfilled");
+        });
+
+        test("should call the API and handle rejected response", async() => {
+            fetchMock.mockResponseOnce(JSON.stringify({message: "Unexpected error"}), {status: 400});
+
+            const store = configureStore({
+                reducer: signupReducer,
+            });
+            const result = await store.dispatch(
+                signupUser({
+                    user_type: "researcher",
+                    first_name: "Artin",
+                    last_name: "Ahmeti",
+                    username: "ahmetiartin143",
+                    email: "artini13@hotmail.com",
+                    password: "strongpass123",
+                    country: "Albania",
+                }) as any
+            );
+
+            const state = store.getState();
+            expect(fetchMock).toHaveBeenCalledWith("https://django-dev.aakscience.com/signup/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    user_type: "researcher",
+                    first_name: "Artin",
+                    last_name: "Ahmeti",
+                    username: "ahmetiartin143",
+                    email: "artini13@hotmail.com",
+                    password: "strongpass123",
+                    country: "Albania",
+                }),
+            });
+
+            expect(state.loading).toBe(false);
+            expect(state.success).toBe(false);
+            expect(state.error).toBe("Unexpected error");
+            expect(result.type).toBe("signup/signupUser/rejected");
+        });
     })
 });
