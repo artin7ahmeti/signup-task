b3967eef376b634fa46b1bbc3d28c992fc19ed33
Artin Ahmeti <artin.ahmeti10@gmail.com>
2025-01-17 14:10:06 +0100

    build: add testing library dependencies

--- signup-form/src/App.test.tsx
+++ signup-form/src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-//import { render, screen } from '@testing-library/react';
-//import App from './App';
+import { render, screen } from '@testing-library/react';
+import { Provider} from "react-redux";
+import SignupForm from "./components/SignupForm";
+import {configureStore} from "redux-mock-store";
 
-//test('renders learn react link', () => {
-//  render(<App />);
- // const linkElement = screen.getByText(/learn react/i);
- // expect(linkElement).toBeInTheDocument();
-//});
+const mockStore = configureStore([]);
+
+describe("SignupForm Component", () => {
+    let store: ReturnType<typeof mockStore>
+
+    beforeEach(() => {
+        store = mockStore ({
+            signup: {
+                formData: {
+                    user_type: "",
+                    first_name: "",
+                    last_name: "",
+                    username: "",
+                    email: "",
+                    password: "",
+                    country: "",
+                },
+                loading: false,
+                success: false,
+                error: null,
+            },
+        });
+});
+test('renders form', () => {
+    render(
+        <Provider store={store}>
+            <SignupForm/>
+        </Provider>
+    );
+
+    expect(screen.getByLabelText(/User Type/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Enter your first name/)).toBeInTheDocument();
+});
+});
